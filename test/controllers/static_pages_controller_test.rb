0cca2bf24da392d5f8ccb5b8c8d5eb7e5c06cb47
USER <user@USER-no-MacBook-Pro.local>
2016-11-16 16:22:33 +0900

    chap3 end

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,12 +3,24 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
+    # getリクエストを送信すれば、
     assert_response :success
+    #「成功」になるはず。
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    # セレクタ(assert_select)では、特定のHTMLタグが存在するかどうかテストする。
+    # 今回は、<title>タグに、Help | ....が存在するかどうかcheckする。
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
 end
