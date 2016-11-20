1e041be38254203384954536d3001dcf5f714ccb
USER <user@USER-no-MacBook-Pro.local>
2016-11-20 15:25:15 +0900

    chap6 end really

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,10 +1,11 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  test "should get new" do
-    get :new　
-    # このテストはまず、newアクションからスタートする。
+
+  test "should get newww" do
+    get :new
     assert_response :success
   end
+  # このテストは、getでnewアクションにアクセスすることから始まる。
 
 end
