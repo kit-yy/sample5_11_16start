2ba1e6be403596b08a38282b6b67508dcb45b97f
USER <user@USER-no-MacBook-Pro.local>
2016-11-20 17:12:04 +0900

    chap7-2end

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
+
+
   def new
     @user = User.new
   end
+  # 新規登録フォームのためのアクション
+
 
   def show
     @user = User.find(params[:id])
   end
+
 
 end
 
