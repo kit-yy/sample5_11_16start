ddcf90b09190ba6ebd5941644f0930bb3fe4ee51
USER <user@USER-no-MacBook-Pro.local>
2016-11-21 12:50:44 +0900

    chap7-4end

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,12 +12,34 @@ class UsersController < ApplicationController
   end
 
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome!!"
+      # ハッシュflashの:successキーに、成功時のメッセージを代入する。
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
 
+# ・ストロングパラメータについて
+# params[:id]のように、一つの要素のみ取り出すものなら大丈夫だが、
+# params[:user]のように、user(name:"", email:"", ...)というように沢山の要素がある場合、
+# httpクライアントからadmin属性などが入れられる可能性があるため危険。
 
+# ・params[:id]とは
 # getリクエストで、params = {:id = "1"}が送られてくる。
 # それを取り出すために、params[:id]と記述する。
 
+# ・debuggerについて
 # デバッグ(debugメソッド)が役に立つ。
 # ＝＞gem byebugを作った。
 # ＝＞debuggerを差し込むと、その画面でコンソールのような画面が使えるようになり、
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "invalid signup information" do
+    get signup_path
+
+    assert_no_difference 'User.count' do
+      post users_path, user:{ name:"",email:"user@invalid", password:"foo", password_confirmation:"bar"}
+    end
+
+    assert_template 'users/new'
+  
+  end
+    # 今回は、ユーザの登録の一連のテストなので、
+    # まずはsignup_path(ユーザ登録ページ)にアクセス。
+
+
+  test "valid signup information" do
+    get signup_path
+    assert_no_difference 'User.count', 1 do
+      post_via_redirect users_path, user:{name: "Example User", email: "user@example.com", password:"password", password_confirmation:"password"}
+    end
+    assert_template 'users/show'
+  end
+  # post_via_redirectメソッドは、POSTリクエスト送信結果を見た後に、
+  # 指定されたURLにリダイレクトするメソッド。
+
+  # asset_template : viewがうまく表示されるかどうかのアサーション
+
+end
