5daf942064a0d4a9093fdd49202053996ebc0c25
USER <user@USER-no-MacBook-Pro.local>
2016-11-22 16:37:23 +0900

    9-1-3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+    if @user.save
+    else
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+    else
+      render 'edit'
+    end
+  end
+
 
   private
     def user_params
