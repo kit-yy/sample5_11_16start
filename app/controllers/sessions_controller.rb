e71032d5c8bf0851341a59a2c1e199657da923fc
USER <user@USER-no-MacBook-Pro.local>
2016-11-22 16:01:27 +0900

    chap8end

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     
     if user && user.authenticate(params[:session][:password])
       log_in user
-      remember(user)
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       # ヘルパーで定義。
       redirect_to user
       # redirect_to user == redirect_to userurl(user)
